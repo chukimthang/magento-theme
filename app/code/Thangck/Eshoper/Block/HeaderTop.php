a5e09c0029cd677741b5693ea8a3aee7cdf35e3d
Chu Kim Thang <chukimthang94@gmail.com>
2017-08-23 17:36:22 +0700

    Template - part 1

---
+++ app/code/Thangck/Eshoper/Block/HeaderTop.php
@@ -0,0 +1,15 @@
+<?Php
+
+namespace Thangck\Eshoper\Block;
+
+class HeaderTop extends \Magento\Framework\View\Element\Template
+{
+    protected function _beforeToHtml()
+    {
+        $config = $this->_scopeConfig;
+        $phone = $config->getValue('general/store_information/phone');
+        $email = $config->getValue('trans_email/ident_general/email');
+        $this->setData('store_phone', $phone);
+        $this->setData('store_email', $email);
+    }
+}
