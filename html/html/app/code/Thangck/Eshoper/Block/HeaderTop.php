dfa96fcd29ddbc6444afecbc2a6f5313ad940e18
Chu Kim Thang <chukimthang94@gmail.com>
2017-08-23 18:24:18 +0700

    Template - part 2

--- html/html/app/code/Thangck/Eshoper/Block/HeaderTop.php
+++ html/html/app/code/Thangck/Eshoper/Block/HeaderTop.php
@@ -9,9 +9,8 @@ class HeaderTop extends \Magento\Framework\View\Element\Template
         $config = $this->_scopeConfig;
         $phone = $config->getValue('general/store_information/phone');
         $email = $config->getValue('trans_email/ident_general/email');
-        $facebook = $config->getValue('social/social_config/facebook');
+
         $this->setData('store_phone', $phone);
         $this->setData('store_email', $email);
-        $this->setData('store_facebook', $facebook);
     }
 }
